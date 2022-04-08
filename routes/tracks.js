40bbcda7e5698bf1eb8dc0b9ecd2b343f9aaa1b0
Hector Serrano <fonto.serrano@gmail.com>
2022-04-07 19:32:51 -0600

    add get and post routes for tracks

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,13 +1,16 @@
 const { Router } = require('express')
+const {
+  getItems,
+  getItem,
+  createItem,
+  updateItem,
+  deleteItem,
+} = require('../controllers/tracks');
 
 const router = Router();
 
-// TODO: http://localhost/tracks GET, POST, DELETE, PUT
-
-router.get('/', (req, res) => {
-  const data = ['hola', 'mundo']
-
-  res.send({ data })
-})
+// TODO: http://localhost/api/tracks GET, POST, DELETE, PUT
+router.get('/', getItems)
+router.post('/', createItem)
 
 module.exports = router;
