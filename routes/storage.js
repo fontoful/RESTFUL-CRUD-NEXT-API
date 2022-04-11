4b779188652e22bc6cd2214f07939ee5ddfbef9c
Hector Serrano <fonto.serrano@gmail.com>
2022-04-10 21:23:10 -0600

    use storage createItem controller

--- routes/storage.js
+++ routes/storage.js
@@ -1,11 +1,10 @@
 const express = require('express')
+const { createItem } = require('../controllers/storage')
 const { Router } = express
 const uploadMiddleware = require('../utils/handleStorage')
 
 const router = Router()
 
-router.post('/', uploadMiddleware.single('myfile'), (req, res) => {
-  res.send({ a: 1 })
-})
+router.post('/', uploadMiddleware.single('myfile'), createItem)
 
 module.exports = router
