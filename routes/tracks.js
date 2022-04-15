d38eeebddeae2a4d127a64f038b03bdae58e27ea
Hector Serrano <fonto.serrano@gmail.com>
2022-04-15 16:31:13 -0600

    add more endpoints to tracks routes

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,5 +1,6 @@
 const { Router } = require('express')
-const { validatorCreateItem } = require('../validators/tracks')
+const { validatorCreateItem, validatorGetItem } = require('../validators/tracks')
+const customHeader = require('../middleware/customHeader') 
 const {
   getItems,
   getItem,
@@ -11,7 +12,19 @@ const {
 const router = Router();
 
 // TODO: http://localhost/api/tracks GET, POST, DELETE, PUT
+/**
+ * Get all the items
+ */
 router.get('/', getItems)
+
+/**
+ * Get a single item
+ */
+router.get('/:id', validatorGetItem, getItem)
+
+/**
+ * Create one item
+ */
 router.post('/', validatorCreateItem, createItem)
 
 module.exports = router;
