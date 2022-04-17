ca56028e06b1523c45e04a12195cc9b86cb512a2
Hector Serrano <fonto.serrano@gmail.com>
2022-04-17 16:07:06 -0600

    create all CRUD routes for storage

--- routes/storage.js
+++ routes/storage.js
@@ -1,10 +1,29 @@
 const express = require('express')
-const { createItem } = require('../controllers/storage')
+const { createItem, getItem, getItems, deleteItem } = require('../controllers/storage');
+const { validatorGetItem } = require('../validators/storage')
 const { Router } = express
 const uploadMiddleware = require('../utils/handleStorage')
 
 const router = Router()
 
+/**
+ * Get all storage items
+ */
+router.get('/', getItems)
+
+/**
+ * Get a single storage item
+ */
+router.get('/:id', validatorGetItem, getItem)
+
+/**
+ * Delete a single storage item
+ */
+router.delete('/:id', validatorGetItem, deleteItem)
+
+/**
+ * Create a single storage item
+ */
 router.post('/', uploadMiddleware.single('myfile'), createItem)
 
 module.exports = router
