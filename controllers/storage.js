63900faacaf739eba6abfacbc23e6d920bb67560
Hector Serrano <fonto.serrano@gmail.com>
2022-04-17 16:07:42 -0600

    controllers for getting an item, all items and deleting an item (storage)

--- controllers/storage.js
+++ controllers/storage.js
@@ -1,5 +1,10 @@
+const fs = require('fs')
+const { matchedData } = require('express-validator');
 const { storageModel } = require('../models');
+const { handleHttpError } = require('../utils/handleError');
+
 const PUBLIC_URL = process.env.PUBLIC_URL
+const MEDIA_PATH = `${__dirname}/../storage`
 
 /**
  * Get me all the stored items
@@ -7,9 +12,12 @@ const PUBLIC_URL = process.env.PUBLIC_URL
  * @param {*} res 
  */
 const getItems = async (req, res) => { 
-  const data = await tracksModel.find({})
-
-  res.send({ data })
+  try {
+    const data = await storageModel.find({})
+    res.send({ data })
+  } catch (error) {
+    handleHttpError(res, 'ERROR_GET_STORAGES')
+  }
 }
 
 /**
@@ -17,7 +25,15 @@ const getItems = async (req, res) => {
  * @param {*} req 
  * @param {*} res 
  */
-const getItem = (req, res) => {}
+const getItem = async (req, res) => {
+  try {
+    const { id } = matchedData(req)
+    const data = await storageModel.findById(id)
+    res.send({ data })
+  } catch (error) {
+    handleHttpError(res, 'ERROR_GET_STORAGE')
+  }
+}
 
 /**
  * Create an item
@@ -39,14 +55,34 @@ const createItem = async (req, res) => {
  * @param {*} req 
  * @param {*} res 
  */
-const updateItem = (req, res) => {}
+const updateItem = async (req, res) => {}
 
 /**
  * Delete an item
  * @param {*} req 
  * @param {*} res 
  */
-const deleteItem = (req, res) => {}
+const deleteItem = async (req, res) => {
+  try {
+    const { id } = matchedData(req) || {}
+    const { filename } = await storageModel.findById(id)
+    const filePath = `${MEDIA_PATH}/${filename}`
+
+    // Do actual deletion (avoid shallow deletion here and use deleteOne)
+    await storageModel.deleteOne({ _id: id })
+
+    fs.unlinkSync(filePath)
+
+    const resData = {
+      filePath,
+      deleted: 1,
+    }
+    
+    res.send({ resData })
+  } catch (error) {
+    handleHttpError(res, 'ERROR_DELETING_STORAGE_ITEM')
+  }
+}
 
 module.exports = {
   getItems,
