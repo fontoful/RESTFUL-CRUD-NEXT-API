89df3b3d28794f6b4918eec4cd66df65554c83bf
Hector Serrano <fonto.serrano@gmail.com>
2022-04-20 00:27:50 -0600

    create session middleware

---
+++ middleware/session.js
@@ -0,0 +1,32 @@
+const { handleHttpError } = require('../utils/handleError')
+const { usersModel } = require('../models')
+const { verifyToken } = require('../utils/handleJwt')
+
+const authMiddleware = async (req, res, next) => {
+  try {
+    const { authorization } = req.headers || {}
+
+    if (!authorization) {
+      handleHttpError(res, 'NO_TOKEN', 401)
+      return
+    }
+
+    const dataToken = verifyToken(authorization)
+
+    if (!dataToken._id) {
+      handleHttpError(res, 'ERROR_ID_TOKEN', 401)
+      return
+    }
+
+    const user = await usersModel.findById(dataToken._id)
+    debugger;
+    req.user = user
+    // inject a user property to the req object if we got this far
+    next()
+
+  } catch (e) {
+    handleHttpError(res, 'NOT_SESSION', 401)
+  }
+}
+
+module.exports = authMiddleware
