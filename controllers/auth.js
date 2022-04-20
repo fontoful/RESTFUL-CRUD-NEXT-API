b94be9ea356e1a58a244804304070e6ea1d4b972
Hector Serrano <fonto.serrano@gmail.com>
2022-04-20 00:30:04 -0600

    create controller for logging to decrypt password and confirm pwd

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,19 +1,52 @@
 const { matchedData } = require("express-validator")
 const { usersModel } = require("../models")
 const { tokenSign } = require("../utils/handleJwt")
-const { encrypt } = require("../utils/handlePassword")
+const { encrypt, compare } = require("../utils/handlePassword")
+const { handleHttpError } = require('../utils/handleError')
 
 const registerController = async (req, res) => {
-  const data = matchedData(req)
-  const hashedPwd = await encrypt(data.password)
-  const body = { ...data, password: hashedPwd }
-  const user = await usersModel.create(body)
-  user.set("password", undefined, { strict: false })
+  try {
+    const data = matchedData(req)
+    const hashedPwd = await encrypt(data.password)
+    const body = { ...data, password: hashedPwd }
+    const user = await usersModel.create(body)
+    user.set("password", undefined, { strict: false })
 
-  res.send({ user, token: tokenSign(user) })
+    res.send({ user, token: tokenSign(user) })
+  } catch (error) {
+    handleHttpError(res, 'ERROR_LOGIN_USER')
+  }
 
+}
+
+const loginController = async (req, res) => {
+  try {
+    const data = matchedData(req)
+    const user = await usersModel.findOne({ email: data.email }).select('password name role email')
+    
+    if (!user) {
+      handleHttpError(res, "USER_DOESNT_EXIST", 404)
+    }
+
+    const hashedPwd = user.password
+    const isCorrectPwd = await compare(data.password, hashedPwd)
+
+    if (!isCorrectPwd) {
+      handleHttpError(res, "INVALID_PASSWORD", 402)
+    }
+
+    user.set('password', undefined, { strict: false })
+
+    res.send({
+      token: tokenSign(user),
+      user
+    })
+  } catch (error) {
+    
+  }
 }
 
 module.exports = {
   registerController,
+  loginController,
 }
