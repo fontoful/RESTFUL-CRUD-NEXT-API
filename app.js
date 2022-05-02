d13c7991f88fc39e8f806872a830154bd7998b35
Hector Serrano <fonto.serrano@gmail.com>
2022-05-01 22:01:00 -0600

    use morganBody in app

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const morganBody = require('morgan-body')
 const cors = require('cors')
 const dbConnect = require('./config/mongo')
 require('dotenv').config();
@@ -9,6 +10,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static('storage'))
+
+morganBody(app, {
+  noColors: true,
+})
 
 const port = process.env.PORT || 3000;
 
