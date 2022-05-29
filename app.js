bfb6625c3bac8538c659b044d14b2b5052799e12
Hector Serrano <fonto.serrano@gmail.com>
2022-05-28 21:48:17 -0600

    send a message to slack when there's an errored request

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const morganBody = require('morgan-body')
 const cors = require('cors')
 const dbConnect = require('./config/mongo')
 require('dotenv').config();
+const loggerStream = require('./utils/handleLogger')
 
 // init app with express
 const app = express();
@@ -13,6 +14,10 @@ app.use(express.static('storage'))
 
 morganBody(app, {
   noColors: true,
+  stream: loggerStream,
+  skip: (req, res) => {
+    return res.statusCode < 400
+  }
 })
 
 const port = process.env.PORT || 3000;
