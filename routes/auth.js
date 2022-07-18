4f030916c0a4f839e09d13db0839fb0ad45eee63
fontoful <fonto.serrano@gmail.com>
2022-07-18 14:56:30 -0600

    temporarily comment out lines that aren't being used (plan on deleting them once confirming they're of no use)

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const { matchedData } = require('express-validator')
-const { usersModel } = require('../models')
-const { compare, encrypt } = require('../utils/handlePassword')
+// const { matchedData } = require('express-validator')
+// const { usersModel } = require('../models')
+// const { compare, encrypt } = require('../utils/handlePassword')
 const { validatorLogin, validatorRegister } = require('../validators/auth')
-const { tokenSign, verifyToken } = require('../utils/handleJwt')
+// const { tokenSign, verifyToken } = require('../utils/handleJwt')
 const { registerController, loginController } = require('../controllers/auth')
 
 /**
