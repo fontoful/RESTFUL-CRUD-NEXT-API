a3cd926a91419d2595081a17b3c1a43b60191316
fontoful <fonto.serrano@gmail.com>
2022-08-30 15:10:34 -0600

    create models for mySql - equivalents to the noSql db

---
+++ models/mysql/tracks.js
@@ -0,0 +1,40 @@
+const { sequelize } = require('../../config/mysql');
+const { DataTypes } = require('sequelize');
+
+// TODO: notice that opposed to the Users.js models file, this one defines Tracks as plural... 🤔 human error, maybe?
+const Tracks = sequelize.define(
+  'tracks',
+  {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    album: {
+      type: DataTypes.STRING,
+    },
+    cover: {
+      type: DataTypes.STRING,
+    },
+    artist_name: {
+      type: DataTypes.STRING,
+    },
+    artist_nickname: {
+      type: DataTypes.STRING,
+    },
+    artist_nationality: {
+      type: DataTypes.STRING,
+    },
+    duration_start: {
+      type: DataTypes.INTEGER,
+    },
+    duration_end: {
+      type: DataTypes.INTEGER,
+    },
+    mediaId: {
+      type: DataTypes.STRING,
+    },
+  },
+  { timestamps: true },
+);
+
+module.exports = Tracks;
