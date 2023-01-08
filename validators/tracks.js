f95ac559779b884bf5f44f171e7c3e9b1d16d6ed
hector serrano <fonto.serrano@gmail.com>
2023-01-07 23:36:05 -0600

    update tracks input validators

--- validators/tracks.js
+++ validators/tracks.js
@@ -8,13 +8,11 @@ const validatorCreateItem = [
   check('name').exists().notEmpty(),
   check('album').exists().notEmpty(),
   check('cover').exists().notEmpty(),
-  check('artist').exists().notEmpty(),
-  check('artist.name').exists().notEmpty(),
-  check('artist.nickname').exists().notEmpty(),
-  check('artist.nationality').exists().notEmpty(),
-  check('duration').exists().notEmpty(),
-  check('duration.start').exists().notEmpty(),
-  check('duration.end').exists().notEmpty(),
+  check('artist_name').exists().notEmpty(),
+  check('artist_nickname').exists().notEmpty(),
+  check('artist_nationality').exists().notEmpty(),
+  check('duration_start').exists().notEmpty(),
+  check('duration_end').exists().notEmpty(),
   check('mediaId').exists().notEmpty(),
   (req, res, next) => validateResults(req, res, next)
 ]
